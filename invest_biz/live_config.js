5d1eb887fc9eb0305a2914fd0f146dc25377600e
intrinsic value <intrinsicvalueequity@gmail.com>
2026-06-22 17:14:00 +0530

    link Investbiz

--- invest_biz/live_config.js
+++ invest_biz/live_config.js
@@ -1,5 +1,5 @@
 var LIVE_CONFIG = {
-    "indexRedirectUrl": "https://premium.intrinsicvalueequity.in/checkout/28180dfc-84a9-488e-854e-9f7958a1b8d7",
+    "indexRedirectUrl": "https://live.intrinsicvalueequity.in/vsl.html",
     "vslCheckoutUrl": "https://premium.intrinsicvalueequity.in/checkout/28180dfc-84a9-488e-854e-9f7958a1b8d7",
     "tyWhatsAppUrl": "https://chat.whatsapp.com/EZzUNyMTkklAxMMQNZw6so"
 };
