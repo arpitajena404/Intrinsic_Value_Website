0e620b14e59938553a30d143f6fa6bd8afc6ad96
Harshit Saraan <harshitsaraan@gmail.com>
2026-06-24 13:55:36 +0530

    vsl link changed

--- vsl/lp.intrinsicvalueequity.in/vsl/vsl_config.js
+++ vsl/lp.intrinsicvalueequity.in/vsl/vsl_config.js
@@ -1,4 +1,4 @@
 var VSL_CONFIG = {
-    "checkoutUrl": "https://youtube.com",
+    "checkoutUrl": "https://premium.intrinsicvalueequity.in/checkout/bc918fe9-de4a-4bc3-8fe4-cb7261a52903/",
     "wistiaMediaId": "4oy11vkpr7"
 };
