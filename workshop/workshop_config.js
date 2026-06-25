11a29c86b03d3d82e8dab490b660abcbd4aa98b8
intrinsic value <intrinsicvalueequity@gmail.com>
2026-06-25 12:20:43 +0530

    workshop date and link

--- workshop/workshop_config.js
+++ workshop/workshop_config.js
@@ -1,7 +1,7 @@
 var WORKSHOP_CONFIG = {
-    "workshopDateText": "5th April, Sunday  | 11:00 AM",
+    "workshopDateText": "28th June, Sunday  | 11:00 AM",
     "countdownTarget": "2026-04-05T11:00:00",
-    "checkoutUrl": "https://premium.intrinsicvalueequity.in/checkout/bc918fe9-de4a-4bc3-8fe4-cb7261a52903",
+    "checkoutUrl": "https://premium.intrinsicvalueequity.in/checkout/d712f080-745a-4171-8487-c03ebfd9e176",
     "youtubeVideo1": "https://www.youtube.com/watch?v=lXyYY8I9e44",
     "youtubeVideo2": "https://www.youtube.com/watch?v=knvVbwdm0TE",
     "youtubeVideo3": "https://www.youtube.com/watch?v=yu7KYacrV80",
@@ -11,5 +11,5 @@ var WORKSHOP_CONFIG = {
     "box2Text": "On Zoom",
     "box3Text": "Actionable Workshop",
     "box4Text": "100% Practical Strategy",
-    "box5Text": "Language: English"
+    "box5Text": "Language: Hinglish"
 };
