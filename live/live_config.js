382d4055dd9d4589be4657886add6e428050d959
Harshit Saraan <harshitsaraan@gmail.com>
2026-06-29 15:34:58 +0530

    live page meta pixel added

---
+++ live/live_config.js
@@ -0,0 +1,5 @@
+var LIVE_CONFIG = {
+    "indexRedirectUrl": "https://premium.intrinsicvalueequity.in/checkout/28180dfc-84a9-488e-854e-9f7958a1b8d7",
+    "vslCheckoutUrl": "https://premium.intrinsicvalueequity.in/checkout/28180dfc-84a9-488e-854e-9f7958a1b8d7",
+    "tyWhatsAppUrl": "https://chat.whatsapp.com/EZzUNyMTkklAxMMQNZw6so"
+};
